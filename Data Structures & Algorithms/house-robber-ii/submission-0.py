3457769148f6300d299340669712d0b49fba0373
Syed Muhammad Ahmed <115739683+s-md-ahmed@users.noreply.github.com>
2026-06-04 13:02:38 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        # Helper function (the code you just mastered!)
+        def rob_linear(houses):
+            rob1, rob2 = 0, 0
+            for n in houses:
+                temp = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2
+        
+        # Apply it to the two possible "lines"
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))#to exclude last index and first index 
