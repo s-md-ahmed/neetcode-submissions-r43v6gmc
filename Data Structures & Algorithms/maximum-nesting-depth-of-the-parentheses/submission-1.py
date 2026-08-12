ba3e41d385c46087fd59468e7acd75ce298e4c61
Syed Muhammad Ahmed <115739683+s-md-ahmed@users.noreply.github.com>
2026-08-12 17:06:28 +0530

    Add: maximum-nesting-depth-of-the-parentheses - submission-1

---
+++ Data Structures & Algorithms/maximum-nesting-depth-of-the-parentheses/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        s1=[]
+        val=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                val+=1
+                
+            if s[i]==')':
+                val-=1
+            s1.append(val)
+        print(s1)
+        fin=int(max(s1,default=0))
+        print(fin)
+        return fin  
